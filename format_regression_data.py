5ea438bec46bdebc58e46d4bcc1c942175b39bff
Sara Nicholas <sara.nicholas13@gmail.com>
2021-05-10 00:25:11 -0400

    added case data to regression csv

--- format_regression_data.py
+++ format_regression_data.py
@@ -28,9 +28,9 @@ def get_dict_from_csv(CSV_IN, category):
     return csv_dict
 
 
-def write_output(states_abbreviations, passengers_from_italy, neighbors_hubs, pageranks_normalized, pageranks_italy):
+def write_output(states_abbreviations, passengers_from_italy, neighbors_hubs, pageranks_normalized, pageranks_italy, cases_mar17, cases_mar31):
     with open(REGRESSION_CSV, 'w') as csvfile:
-        fieldnames = ['STATE', 'STATE_ABBREVIATION', 'PASSENGERS_FROM_ITALY_SCALED', 'NEIGHBORS_HUBS_ITALY', 'RANK_NORMALIZED', 'RANK_ITALY']
+        fieldnames = ['STATE', 'STATE_ABBREVIATION', 'PASSENGERS_FROM_ITALY_SCALED', 'NEIGHBORS_HUBS_ITALY', 'RANK_NORMALIZED', 'RANK_ITALY', 'CASES_MAR_17', 'CASES_MAR_31']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for state in states_abbreviations.keys():
@@ -38,12 +38,16 @@ def write_output(states_abbreviations, passengers_from_italy, neighbors_hubs, pa
             neighbor = neighbors_hubs[state] if state in neighbors_hubs.keys() else 0
             rank_normalized = pageranks_normalized[state] if state in pageranks_normalized.keys() else 0
             rank_italy = pageranks_italy[state] if state in pageranks_italy else 0
+            cases_17 = cases_mar17[state] if state in cases_mar17.keys() else 0
+            cases_31 = cases_mar31[state] if state in cases_mar31.keys() else 0
             writer.writerow({'STATE': states_abbreviations[state],
                             'STATE_ABBREVIATION': state,
                             'PASSENGERS_FROM_ITALY_SCALED': from_italy,
                             'NEIGHBORS_HUBS_ITALY': neighbor,
                             'RANK_NORMALIZED': rank_normalized,
-                            'RANK_ITALY': rank_italy})
+                            'RANK_ITALY': rank_italy,
+                            'CASES_MAR_17': cases_17,
+                            'CASES_MAR_31': cases_31})
 
 def format_regression_data_main():
     states_abbreviations = get_states_abbreviations_from_csv()
@@ -51,5 +55,8 @@ def format_regression_data_main():
     neighbors_hubs = get_dict_from_csv(NEIGHBORS_HUBS_CSV, 'NEIGHBORS_HUBS_ITALY')
     pageranks_normalized = get_dict_from_csv(STATE_PAGERANKS_CSV, 'RANK_NORMALIZED')
     pageranks_italy = get_dict_from_csv(STATE_PAGERANKS_CSV, 'RANK_ITALY')
+    cases_mar17 = get_dict_from_csv(CASES_CSV, 'CASES_MAR_17')
+    cases_mar31 = get_dict_from_csv(CASES_CSV, 'CASES_MAR_31')
 
-    write_output(states_abbreviations, passengers_from_italy, neighbors_hubs, pageranks_normalized, pageranks_italy)
+    write_output(states_abbreviations, passengers_from_italy, neighbors_hubs,
+                pageranks_normalized, pageranks_italy, cases_mar17, cases_mar31)
